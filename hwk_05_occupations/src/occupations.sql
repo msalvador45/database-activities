e44845554cb8c0a640e992e4d19a254dc6adecfd
Migui <miguelsalvador355@gmail.com>
2023-03-10 10:44:45 -0700

    worked on hw5 still not done tho

--- hwk_05_occupations/src/occupations.sql
+++ hwk_05_occupations/src/occupations.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student: 
+-- Student: Miguel A. Salvador Tzoni
 -- Description: a database of occupations
 
 CREATE DATABASE occupations;
@@ -10,10 +10,20 @@ CREATE DATABASE occupations;
 DROP TABLE IF EXISTS Occupations;
 
 -- TODO: create table Occupations
+CREATE TABLE Occupations(
+    code VARCHAR(10) PRIMARY KEY,
+    occupation VARCHAR(300),
+    jobFamily VARCHAR(300)
+);
+
+\d Occupations
 
 -- TODO: populate table Occupations
+\copy Occupations
+    (code, occupation, job) from /home/occupations.csv DELIMITER ',' CSV HEADER;
 
 -- TODO: a) the total number of occupations (expect 1016).
+SELECT COUNT(*) FROM Occupations;
 
 -- TODO: b) a list of all job families in alphabetical order (expect 23).
 
