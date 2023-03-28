a5a1dcf6421a2161083067c1f03637945ea37e4c
Migui <miguelsalvador355@gmail.com>
2023-03-28 11:38:25 -0600

    installed psychopg2 and started activry  12 and 13

--- activity_13_db_access/src/hr.sql
+++ activity_13_db_access/src/hr.sql
@@ -2,3 +2,15 @@
 -- Instructor: Thyago Mota
 -- Description: The hr database
 
+CREATE DATABASE hr;
+
+\c hr
+
+CREATE TABLE Employees (
+ id INT NOT NULL PRIMARY KEY,
+ name VARCHAR(35) NOT NULL,
+ sal INT ); 
+
+INSERT INTO Employees VALUES
+ ( 1, 'Sam Mai Tai', 35000 ),
+ ( 2, 'Morbid Mojito', 65350 );
