a359fe589d9a7ed60ee850289e0e97ec296fb873
Migui <miguelsalvador355@gmail.com>
2023-03-30 11:34:06 -0600

    trouble w/ sql injections

--- activity_14_psycopg/testdb.py
+++ activity_14_psycopg/testdb.py
@@ -9,5 +9,13 @@ conn = psycopg2.connect(**params)
 if conn: 
     print('Connection to Postgres database ' + params['dbname'] + ' was successful!')
 
+    # TODO: ask the user for a hotel's name and then using prepared staement 
+    # show the information about the hotel
+    name = input("? ")
+    cur = conn.curosr()
+    sql = "SELECT * FROM hotels WHERE name = '%s'"  #%s is a string look up more about this
+
+    
+    
     print('Bye!')
     conn.close()
