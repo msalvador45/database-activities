2eff6bdc5b3e2d849fcdda9a092bebacdf8acb8d
Migui <miguelsalvador355@gmail.com>
2023-04-27 17:31:32 -0600

    minor work

--- db_03_booking/src/booking.py
+++ db_03_booking/src/booking.py
@@ -1,7 +1,7 @@
 """
 CS3810: Principles of Database Systems
 Instructor: Thyago Mota
-Student(s): 
+Student(s): Miguel A. Salvador Tzoni
 Description: A room reservation system
 """
 
--- db_03_booking/src/booking.sql
+++ db_03_booking/src/booking.sql
@@ -86,7 +86,6 @@ GRANT ALL ON SEQUENCE users_user_seq TO booking;
 --     3 | 2023-06-23 | E      | 14:00:00 | 16:00:00 | AES-220 | Morbid Mojito
 --     2 | 2023-06-12 | D      | 12:00:00 | 14:00:00 | AES-210 | Sam Mai Tai
 --     1 | 2023-05-15 | C      | 10:00:00 | 12:00:00 | AES-210 | Sam Mai Tai
-
 CREATE VIEW ReservationsView AS 
 SELECT code, date, A.period, start, "end", CONCAT(abbr,' ', room) AS room, name 
 FROM Reservations A 
